7e576524fe111e2fc6e52f053a1ce7eecf6e8087
pedroguaillas <peter.tufi@gmail.com>
2024-03-27 12:01:20 -0500

    Estilos notas negritra

--- src/components/InstitutionItem.jsx
+++ src/components/InstitutionItem.jsx
@@ -2,18 +2,37 @@ import React from 'react';
 import { View } from "react-native";
 import StyleText from './StyleText';
 
+const InstitutionStats = (props) => (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <View>
+            <StyleText fontWeight='bold'>Matemática</StyleText>
+            <StyleText>{props.imat}</StyleText>
+        </View>
+        <View>
+            <StyleText fontWeight='bold'>Lenguaje</StyleText>
+            <StyleText>{props.ilyl}</StyleText>
+        </View>
+        <View>
+            <StyleText fontWeight='bold'>Ciencias</StyleText>
+            <StyleText>{props.icn}</StyleText>
+        </View>
+        <View>
+            <StyleText fontWeight='bold'>Estudios</StyleText>
+            <StyleText>{props.ies}</StyleText>
+        </View>
+    </View>
+)
+
 const InstitutionItem = (props) => {
     return (
         <View>
             <StyleText fontSize='subheading' fontWeight='bold'>Código: {props.amie}</StyleText>
-            <StyleText>Institución: {props.nm_inst}</StyleText>
-            <StyleText color='primary'>Provincia: {props.nm_prov}</StyleText>
+            <StyleText color='primary'>Institución: {props.nm_inst}</StyleText>
+            <StyleText color='secondary'>Provincia: {props.nm_prov}</StyleText>
             <StyleText>Ciudad: {props.nm_prov}</StyleText>
-            <StyleText fontWeight='bold'>Cod2:{props.codigo}</StyleText>
-            <StyleText>Matemática: {props.imat}</StyleText>
-            <StyleText>Lenguaje: {props.ilyl}</StyleText>
-            <StyleText>Ciencias: {props.icn}</StyleText>
-            <StyleText>Estudios: {props.ies}</StyleText>
+            <StyleText>Calificaciones:{props.codigo}</StyleText>
+            <StyleText fontWeight='bold'>NOTAS</StyleText>
+            <InstitutionStats {...props} />
         </View>
     );
 }
